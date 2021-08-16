dc17b0eafa8a576b64c5cc04fb687ed9d13a11a4
tlam22 <tszlam22@gmail.com>
2021-08-16 17:25:42 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // run `node index.js` in the terminal
 const Taxee = require('taxee-tax-statistics');
-console.log(Taxee.default['2020']['alabama'].single.income_tax_brackets);
+//console.log(Taxee.default['2020']['alabama'].single.income_tax_brackets);
+console.log(Taxee.default['2020'].federal)
 
